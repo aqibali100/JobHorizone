db50747aa1df57704d393eba755a9308ee86edab
Aqib Ali <aqib28000@gmail.com>
2024-09-09 22:18:30 +0500

    add company and callout section

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,4 +1,12 @@
 import React from 'react'
+import Capitalone from '../assets/images/Capitalone.png'
+import airbnb from '../assets/images/airbnb.png'
+import shipBob from '../assets/images/Shipbob.png'
+import discord from '../assets/images/Discord.png'
+import walmart from '../assets/images/Walmart.png'
+import dropbox from '../assets/images/Dropbox.png'
+import slack from '../assets/images/Slack.png'
+import dribble from '../assets/images/Dribble.png'
 
 const About = () => {
     return (
@@ -13,6 +21,59 @@ const About = () => {
                                     </h1>
                                     <p>We connect job seekers with opportunities that match their skills<br></br> and aspirations. </p>
                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="company-bg mt-5 pt-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="company-text text-center">
+                                <h1>More Than <span>50+ companies</span> sponsorship with us</h1>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row pt-5">
+                        <div class="col-lg-3 col-md-6 mb-5">
+                            <div class="company-img">
+                                <img src={airbnb} alt='company-img'></img>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-5">
+                            <div class="company-img">
+                                <img src={shipBob} alt='company-img'></img>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-5">
+                            <div class="company-img">
+                                <img src={discord} alt='company-img'></img>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-5">
+                            <div class="company-img">
+                                <img src={walmart} alt='company-img'></img>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-5">
+                            <div class="company-img">
+                                <img src={Capitalone} alt='company-img'></img>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-5">
+                            <div class="company-img">
+                                <img src={dropbox} alt='company-img'></img>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-5">
+                            <div class="company-img">
+                                <img src={slack} alt='company-img'></img>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 mb-5">
+                            <div class="company-img">
+                                <img src={dribble} alt='company-img'></img>
                             </div>
                         </div>
                     </div>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -9,6 +9,14 @@ import resumeImg from '../assets/images/boy.png'
 import jobImg from '../assets/images/job.jpg'
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import { Link } from 'react-router-dom';
+import Capitalone from '../assets/images/Capitalone.png'
+import airbnb from '../assets/images/airbnb.png'
+import shipBob from '../assets/images/Shipbob.png'
+import discord from '../assets/images/Discord.png'
+import walmart from '../assets/images/Walmart.png'
+import dropbox from '../assets/images/Dropbox.png'
+import slack from '../assets/images/Slack.png'
+import dribble from '../assets/images/Dribble.png'
 const Home = () => {
   return (
     <>
@@ -189,7 +197,7 @@ const Home = () => {
                 </div>
                 <div className="job-post-date">
                   <p>Posted : 1 day ago</p>
-                  <p className='job-type bg-primary p-2'>Part-Time</p>
+                  <p className='job-type p-2'>Apply Now</p>
                 </div>
               </div>
             </div>
@@ -209,7 +217,7 @@ const Home = () => {
                 </div>
                 <div className="job-post-date">
                   <p>Posted : 1 day ago</p>
-                  <p className='job-type bg-primary p-2'>Part-Time</p>
+                  <p className='job-type p-2'>Apply Now</p>
                 </div>
               </div>
             </div>
@@ -229,7 +237,7 @@ const Home = () => {
                 </div>
                 <div className="job-post-date">
                   <p>Posted : 1 day ago</p>
-                  <p className='job-type bg-primary p-2'>Part-Time</p>
+                  <p className='job-type p-2'>Apply Now</p>
                 </div>
               </div>
             </div>
@@ -249,7 +257,7 @@ const Home = () => {
                 </div>
                 <div className="job-post-date">
                   <p>Posted : 1 day ago</p>
-                  <p className='job-type bg-primary p-2'>Part-Time</p>
+                  <p className='job-type p-2'>Apply Now</p>
                 </div>
               </div>
             </div>
@@ -269,7 +277,7 @@ const Home = () => {
                 </div>
                 <div className="job-post-date">
                   <p>Posted : 1 day ago</p>
-                  <p className='job-type bg-primary p-2'>Part-Time</p>
+                  <p className='job-type p-2'>Apply Now</p>
                 </div>
               </div>
             </div>
@@ -289,7 +297,7 @@ const Home = () => {
                 </div>
                 <div className="job-post-date">
                   <p>Posted : 1 day ago</p>
-                  <p className='job-type bg-primary p-2'>Part-Time</p>
+                  <p className='job-type p-2'>Apply Now</p>
                 </div>
               </div>
             </div>
@@ -374,6 +382,71 @@ const Home = () => {
                   Your CV at JobHorizon</h1>
                 <p>In the subject line of your email, write your name, the description
                   of the position and its reference number. If you did not find the vacancy on the employer’s website,</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="company-bg mt-5 pt-5">
+        <div class="container">
+          <div class="row">
+            <div class="col">
+              <div class="company-text text-center">
+                <h1>More Than <span>50+ companies</span> sponsorship with us</h1>
+              </div>
+            </div>
+          </div>
+          <div class="row pt-5">
+            <div class="col-lg-3 col-md-6 mb-5">
+              <div class="company-img">
+                <img src={airbnb} alt='company-img'></img>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+              <div class="company-img">
+                <img src={shipBob} alt='company-img'></img>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+              <div class="company-img">
+                <img src={discord} alt='company-img'></img>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+              <div class="company-img">
+                <img src={walmart} alt='company-img'></img>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+              <div class="company-img">
+                <img src={Capitalone} alt='company-img'></img>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+              <div class="company-img">
+                <img src={dropbox} alt='company-img'></img>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+              <div class="company-img">
+                <img src={slack} alt='company-img'></img>
+              </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-5">
+              <div class="company-img">
+                <img src={dribble} alt='company-img'></img>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="callout-bg mt-5">
+        <div class="container">
+          <div class="row">
+            <div class="col d-flex justify-content-end">
+              <div class="callout-text pt-5">
+                <h1>Make <span>Recruiting</span> Your Competitive Advantage</h1>
+                <p>Talent is a top priority for all startup founders and executives. Jobify offers a way to completely optimize your entire recruiting process. Find better candidates, conduct more focused interviews, and make data-driven hiring decisions.</p>
               </div>
             </div>
           </div>
